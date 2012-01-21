55a55f825b63a7e38061b8ba58da9a2da71aa814
Terry Matula <terrymatula@gmail.com>
2012-01-21 01:33:28 -0600

    First commit for Simple Facebook Login

---
+++ facebook.php
@@ -0,0 +1,87 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Name:  Simple Facebook Codeigniter Login
+ *
+ * Author: Terry Matula
+ *         terrymatula@gmail.com
+ *         @terrymatula
+
+ * Created:  03.31.2011
+ *
+ * Description:  An easy way to use Facebook to login
+ *
+ * Requirements: PHP5 or above
+ *
+ */
+class Facebook extends CI_Controller
+{
+
+    public $appid;
+    public $apisecret;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // replace these with Application ID and Application Secret.
+        $this->appid = '12345';
+        $this->apisecret = '123abc123';
+    }
+
+    /**
+     * if you have a Facebook login button on your site, link it here
+     */
+    public function index()
+    {
+        // set the page you want Facebook to send the user back to
+        $callback = site_url('facebook/confirm');
+        // create the FB auth url to redirect the user to. 'scope' is
+        // a comma sep list of the permissions you want. then direct them to it
+        $url = "https://graph.facebook.com/oauth/authorize?client_id={$this->appid}&redirect_uri={$callback}&scope=email,publish_stream";
+        redirect($url);
+    }
+
+    /**
+     * Get tokens from FB then exchanges them for the User login tokens
+     */
+    public function confirm()
+    {
+        // get the code from the querystring
+        $redirect = site_url('facebook/confirm');
+        $code = $this->input->get('code');
+        if ($code)
+        {
+            // now to get the auth token. '__getpage' is just a CURL method
+            $gettoken = "https://graph.facebook.com/oauth/access_token?client_id={$this->appid}&redirect_uri={$redirect}&client_secret={$this->apisecret}&code={$code}";
+            $return = $this->__getpage($gettoken);
+            // put the token into the $access_token variable
+            parse_str($return);
+            // now you can save the token to a database, and use it to access the user's graph
+            // for example, this will return all their basic info.  check the FB Dev docs for more.
+            $infourl = "https://graph.facebook.com/me?access_token=$access_token";
+            $return = $this->__getpage($infourl);
+            $info = json_decode($return);
+            print_r($info);
+        }
+    }
+
+    /**
+     * CURL method to interface with FB API
+     * @param string $url
+     * @return json 
+     */
+    private function __getpage($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $return = curl_exec($ch);
+        curl_close($ch);
+        return $return;
+    }
+
+}
